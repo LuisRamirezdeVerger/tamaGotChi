81d13d5e6d5abf5b8aa3aa6344862955e0dec79b
Luis RdV <job.wito.uk@gmail.com>
2021-11-12 17:15:50 +0000

    tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,89 +1,125 @@
-let selectedOption = ["play", "feed", "hydrate"];
+const inquirer = require("inquirer")
+const {qstns} = require ("./questions")
+const {animal} = require ("./classes")
 
-class animal{
-constructor(type, name, age, health) {
-
-    this._type = type,
-    this._mood = 100, 
-    this._hunger = 100,
-    this._thirst = 100,
-    this._name = "";
-        // this.indexMood = 0;
-        // this.indexHunger = 0;
-        // this.indexThirst = 0;
-
-    }
-};
+let selectedOption = ["play", "feed", "drink"];
 
 //The game
-
 const gameLoop = () => {
-     
-    if( this.mood < 50){
-         console.log("Your pet is" + configs.mood[0])
-    }else{
-         console.log(`${type[i]})is happy`)
+    
+        if (animal.health < 0) {
+            console.log("Your pet has died")
+            return
+        } else if(animal.hunger < 0) {
+            console.log("Your pet has died")
+            return
+        } else if (animal.thirst < 0){
+            console.log ("Your pet has died")
+            return
+        }
+
     }    
+        
+
+        //CHECK FROM HERE AAP.JS ABD CLASSES.JS!
+        inquirer
+            .prompt([
+                {
+                    type: "rawlist",
+                    name: "action",
+                    message: "What would you like to do?",
+                    choices: ["Feed", "Hydrate", "Play"],
+                },
+            ])
+            .then((answers) => {
+                if (answers.action === "Feed") {
+                    animal.feed()
+                    console.log("Your buddy is eating!")
+                } else if (answers.action === "Play") {
+                    animal.play()
+                    console.log("Your buddy is playing!")
+                } else {
+
+                }
+                console.table(animal)
+            })
+            .then(() => gameLoop())
     
-    if(hunger==stuffed){
-     console.log("Your pet is" + configs.mood[1])
-    }else{
-      feed(type[i])
-    }
     
     
-    if (type === type[0] && mood === mood[0]) {
-        console.log("The cat is bored")
-    } else {
-        console.log("The cat")
-    }
-    // Status depending on selectedOption -.-
-    if (selectedOption === "play") {
-        this.mood + 10 && this.hunger + 15;
-    } else {
-        this.mood - 10;
-    }
-    if (selectedOption === "feed") {
-        this.hunger - 10 && mood + 3;
-    } else {
-        this.hunger + 10;
-    }
-    if (selectedOption === "hydrate") {
-        this.thirst - 10 && mood + 3;
-    } else {
-        this.thirst + 10;
-    }
+    init()
+//     if (pet.hunger< 0) {
+//         console.log("Your pet died")
+//         return
+//     }
+
+//     inquirer
+//         .prompt([
+//             {
+//                 type: "rawlist",
+//                 name: "action",
+//                 message: "What would you like to do?",
+//                 choices: ["Feed", "Hydrate", "Play"],
+//             },
+//         ])
+//         .then((answers) => {
+//             if (answers.action === "Feed") {
+//                 animal.feed()
+//             } else if (answers.action === "Play") {
+//                 console.log("Playing...")
+//             } else {
+//                 console.log("Drinking...")
+//             }
+
+//             console.table(animal)
+//         })
+//         .then(() => gameLoop())
+
+
+
+// init()
+    
+    //{
+    //  console.log("Your pet is" + configs.mood[1])
+    // }else{
+    //   feed(type[i])
+    // }
+    
+    
+    // if (type === type[0] && mood === mood[0]) {
+    //     console.log("The cat is bored")
+    // } else {
+    //     console.log("The cat")
+    // }
+
+    
+    // // Status depending on selectedOption -.-
+    // if (selectedOption === "play") {
+    //     this.mood + 10 && this.hunger + 15;
+    // } else {
+    //     this.mood - 10;
+    // }
+    // if (selectedOption === "feed") {
+    //     this.hunger - 10 && mood + 3;
+    // } else {
+    //     this.hunger + 10;
+    // }
+    // if (selectedOption === "drinking") {
+    //     this.thirst - 10 && mood + 3;
+    // } else {
+    //     this.thirst + 10;
+    // }
    
-    gameLoop();
-}
+    // gameLoop();
+
 
 //Conditions of the pet
-let configs = {
-    type:["cat", "dog", "rabbit"],
-    name: [],
-    age :["young", "adult", "old"],
-    health :["healthy", "ill"],
+// let configs = {
+//     type:["cat", "dog", "rabbit"],
+//     name: [],
+//     age :["young", "adult", "old"],
+//     health :["healthy", "ill"],
 //     mood: ["bored", "happy"],
-//     hydrate:["thirsty", "not thirsty"],
+//     drinking:["thirsty", "not thirsty"],
 //     feed:["hungry", "stuffed"]
-}
-
-let cat = new animal(configs.type[0], name, age, health);
-
-
-// mood = 50;
-// console.log(this._mood);
-
-// create a animal class with instances of it
-
-// function Pet(name, mood, hunger, age, health) {
-// 	this.mood = mood;
-// 	this.hunger = hunger;
-// 	this.age = age;
-// 	this.health = health;
-// 	this.name = name;
-//     this.indexAge = 0;
-//     this.indexMood = 0;
-//     this.indexHunger = 0;
-//     this.indexHealth = 0;
-// }
+//}
