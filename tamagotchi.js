e23afc35cb0837354e88ce12407223ad3e5cc125
LuisRamirezdeVerger <92444676+LuisRamirezdeVerger@users.noreply.github.com>
2021-11-14 03:23:27 +0000

    Update tamagotchi.js

--- tamagotchi.js
+++ tamagotchi.js
@@ -13,9 +13,7 @@ const init = () => {
         .then (() => gameLoop())
 } 
 
-//The game
 const gameLoop = () => {
-    
         if (partner.health < 0) {
             console.log("Your partner has died")
             return
@@ -25,11 +23,7 @@ const gameLoop = () => {
         } else if (partner.thirst < 0){
             console.log ("Your pet has died")
             return
-        }
-
-        
-        
-        //CHECK FROM HERE AAP.JS ABD CLASSES.JS!
+        }        
         inquirer
             .prompt([
                 {
@@ -52,86 +46,7 @@ const gameLoop = () => {
                 } else if (answers.action === "Check") {
                     console.table(partner)
                 }
-
-                // partner.status()
             })
             .then(() => gameLoop())
-    
-    
         }
     init()
-//     if (pet.hunger< 0) {
-//         console.log("Your pet died")
-//         return
-//     }
-
-//     inquirer
-//         .prompt([
-//             {
-//                 type: "rawlist",
-//                 name: "action",
-//                 message: "What would you like to do?",
-//                 choices: ["Feed", "Hydrate", "Play"],
-//             },
-//         ])
-//         .then((answers) => {
-//             if (answers.action === "Feed") {
-//                 animal.feed()
-//             } else if (answers.action === "Play") {
-//                 console.log("Playing...")
-//             } else {
-//                 console.log("Drinking...")
-//             }
-
-//             console.table(animal)
-//         })
-//         .then(() => gameLoop())
-
-
-
-// init()
-    
-    //{
-    //  console.log("Your pet is" + configs.mood[1])
-    // }else{
-    //   feed(type[i])
-    // }
-    
-    
-    // if (type === type[0] && mood === mood[0]) {
-    //     console.log("The cat is bored")
-    // } else {
-    //     console.log("The cat")
-    // }
-
-    
-    // // Status depending on selectedOption -.-
-    // if (selectedOption === "play") {
-    //     this.mood + 10 && this.hunger + 15;
-    // } else {
-    //     this.mood - 10;
-    // }
-    // if (selectedOption === "feed") {
-    //     this.hunger - 10 && mood + 3;
-    // } else {
-    //     this.hunger + 10;
-    // }
-    // if (selectedOption === "drinking") {
-    //     this.thirst - 10 && mood + 3;
-    // } else {
-    //     this.thirst + 10;
-    // }
-   
-    // gameLoop();
-
-
-//Conditions of the pet
-// let configs = {
-//     type:["cat", "dog", "rabbit"],
-//     name: [],
-//     age :["young", "adult", "old"],
-//     health :["healthy", "ill"],
-//     mood: ["bored", "happy"],
-//     drinking:["thirsty", "not thirsty"],
-//     feed:["hungry", "stuffed"]
-//}
